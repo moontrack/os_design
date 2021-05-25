711c6ea4735eee855502e77d931950f111dda160
moontrack <731590917@qq.com>
2021-05-25 10:52:09 +0800

    first

--- main.cpp
+++ main.cpp
@@ -3,6 +3,8 @@
 #include <vector>
 #include <cstdio>
 using namespace std;
+typedef unsigned int uint;
+#define cmdHelp "help"
 #define cmdCreateFile "createFile"
 #define cmdDeleteFile "deleteFile"
 #define cmdCreateDir "createDir"
@@ -12,6 +14,7 @@ using namespace std;
 #define cmdCp "cp"
 #define cmdSum "sum"
 #define cmdCat "cat"
+#define cmdExit "exit"
 
 #define MAX_STORAGE_SIZE 16777216 //最大磁盘空间 16 * 1024 * 1024 = 16777216
 #define BLOCK_SIZE 1024
@@ -20,71 +23,104 @@ using namespace std;
 #define INDIRECT_BLOCK_NUM 1
 
 #pragma region Struct
-class INODE
+// size=60
+struct INODE
 {
-
+	uint ino;                                   // INODE号
+	uint direct[DIRECT_BLOCK_NUM];              // 直接
+	uint indirect[INDIRECT_BLOCK_NUM];          // 间接
+	uint links;                                 // 链接数
+	uint size;                                  // 大小
+	uint createTime;                            // 时间
+	int fmode;                                  // 文件类型
 };
 #pragma endregion
 
 
 
 #pragma region COMMAND
-// 欢迎信息
-void welcomeMsg()
+// 启动载入磁盘
+void Start()
+{
+	printf("开始载入...\n");
+	// function
+	printf("载入完成\n");
+}
+// 帮助信息
+void Help()
 {
 
 }
+// 欢迎信息
+void WelcomeMsg()
+{
+	// function
+	Help();
+}
+// 启动
+void Welcome()
+{
+	Start();
+	WelcomeMsg();
+}
 // 创建文件 createFile fileName fileSize (KB)
-void createFile(char* fileName, char* fileSize)
+void CreateFile(char* fileName, char* fileSize)
 {
 
 }
 // 删除文件 deleteFile fileName
-void deleteFile(char* fileName)
+void DeleteFile(char* fileName)
 {
 
 }
 // 创建文件夹 createDir dirName
-void createDir(char* dirName)
+void CreateDir(char* dirName)
 {
 
 }
 // 删除文件夹 deleteDir dirName
-void deleteDir(char* dirName)
+void DeleteDir(char* dirName)
 {
 
 }
 // 改变当前工作路径 changeDir path
-void changeDir(char* path)
+void ChangeDir(char* path)
 {
 
 }
 // 列出当前目录下的文件和子目录及对应信息(SIZE CREATETIME) dir
-void dir()
+void Dir()
 {
 
 }
 // 复制文件 cp file1 file2
-void cp(char* file1, char* file2)
+void Cp(char* file1, char* file2)
 {
 
 }
 // 显示空间使用信息 sum
-void sum()
+void Sum()
 {
 
 }
 // 打印文件内容 cat file
-void cat(char* file)
+void Cat(char* file)
 {
 
 }
-// 解析并处理cmd
-void parse(char* cmd)
+// 退出，将内容写入磁盘
+void Exit()
+{
+
+}
+// 解析并处理cmd 
+// 退出返回0
+bool parse(char* cmd)
 {
 	// cmd分段
 	static vector<char*> vec;
-	static bool head = 1;
+	static bool head;
+	head = 1;
 	vec.clear();
 	for (int i = 0; cmd[i]; i++)
 	{
@@ -103,8 +139,12 @@ void parse(char* cmd)
 	{
 		printf("%s\n", item);
 	}
-	if (vec.size() == 0) return; // 无内容返回
-	if (strcmp(vec[0], cmdCreateFile) == 0)
+	if (vec.size() == 0) return 1; // 无内容返回
+	if (strcmp(vec[0], cmdHelp) == 0)
+	{
+		Help();
+	}
+	else if (strcmp(vec[0], cmdCreateFile) == 0)
 	{
 
 	}
@@ -140,16 +180,32 @@ void parse(char* cmd)
 	{
 
 	}
+	else if (strcmp(vec[0], cmdExit) == 0)
+	{
+		printf("结束运行，保存数据中...\n");
+
+		printf("保存完毕\n");
+		return 0;
+	}
 	else
 	{
 		printf("无法识别命令%s\n", vec[0]);
 	}
+	return 1;
 }
 #pragma endregion
 
 int main()
 {
-	char s[100];
-	scanf("%[^\n]", s);
-	parse(s);
+	// test //
+	printf("INODE size = %d\n", sizeof(INODE));
+	// end  //
+
+	Welcome();
+	char input[100];
+	while (true)
+	{
+		scanf("%[^\n]", input); getchar();
+		if (!parse(input)) break;
+	}
 }
